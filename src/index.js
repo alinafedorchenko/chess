4eb7b00ff44b83da576c28fe71c60092e298c02d
alinafedorchenko <alinafedorchenko@i.ua>
2016-10-26 16:10:17 +0300

    add webpack to project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './containers/App'
+import './styles/app.css'
+
+render(
+    <App />,
+    document.getElementById('root')
+)
